3f02d12b630af615705802456ff5e3259c3f6e63
Nikhil Ser <96014562+ajithaajitha952@users.noreply.github.com>
2024-12-21 21:28:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
-MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya", // Add your mongodb url
+SESSION_ID: process.env.SESSION_ID || "PmZmFKqY#LICZTJq9rysEIePiUQuTYSk7_g23TIspqbMw5ZnPcVg", // Add your session id
+MONGODB: process.env.MONGODB || "mongodb+srv://nikhilbiju995:4ZbwWzKWLOwys3wt@cluster0.3gyuu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", // Add your mongodb url
 };
